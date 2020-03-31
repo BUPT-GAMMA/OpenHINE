e7fb624261e7147d422846171942726e23a440a6
Theheavens <34649403+Theheavens@users.noreply.github.com>
2020-03-31 20:40:04 +0800

    -
    .

--- test.py
+++ test.py
@@ -10,95 +10,89 @@ import warnings
 import argparse
 
 
-def evaluate_cluster(embedding_matrix, n_label):
-    embedding_list = embedding_matrix.tolist()
+class evaluation:
+    def __init__(self, seed, label_file):
+        self.seed = seed
+        self.node_label = {}
+        self.label = {}
+        la = []
+        with open(label_file) as f:
+            for line in f:
+                i, l = line.strip().split()
+                self.label[i] = int(l)
+                la.append(int(l))
+        self.n_label = len(set(la))
 
-    X = []
-    Y = []
-    for p in label:
-        X.append(embedding_list[p])
-        Y.append(label[p])
+    def evaluate_cluster(self, embedding_list):
+        X = []
+        Y = []
+        for p in self.label:
+            X.append(embedding_list[p])
+            Y.append(self.label[p])
 
-    Y_pred = KMeans(n_label, random_state=0).fit(np.array(X)).predict(X)
-    nmi = normalized_mutual_info_score(np.array(Y), Y_pred)
-    ari = adjusted_rand_score(np.array(Y), Y_pred)
-    return nmi, ari
+        Y_pred = KMeans(self.n_label, random_state=self.seed).fit(np.array(X)).predict(X)
+        nmi = normalized_mutual_info_score(np.array(Y), Y_pred)
+        ari = adjusted_rand_score(np.array(Y), Y_pred)
+        return nmi, ari
 
 
-def evaluate_clf(embedding_matrix, seed):
-    embedding_list = embedding_matrix.tolist()
+    def evaluate_clf(self, embedding_list):
+        X = []
+        Y = []
+        for p in self.label:
+            X.append(embedding_list[p])
+            Y.append(self.label[p])
 
-    X = []
-    Y = []
-    for p in label:
-        X.append(embedding_list[p])
-        Y.append(label[p])
+        X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state=self.seed)
 
-    X_train, X_test, Y_train, Y_test = train_test_split(X, Y, test_size=0.2, random_state= 0)
+        LR = LogisticRegression()
+        LR.fit(X_train, Y_train)
+        Y_pred = LR.predict(X_test)
 
-    LR = LogisticRegression()
-    LR.fit(X_train, Y_train)
-    Y_pred = LR.predict(X_test)
-
-    micro_f1 = f1_score(Y_test, Y_pred, average='micro')
-    macro_f1 = f1_score(Y_test, Y_pred, average='macro')
-    return micro_f1, macro_f1
+        micro_f1 = f1_score(Y_test, Y_pred, average='micro')
+        macro_f1 = f1_score(Y_test, Y_pred, average='macro')
+        return micro_f1, macro_f1
 
 
 def str_list_to_float(str_list):
     return [float(item) for item in str_list]
 
-
+def load_emd(filename):
+    embedding_matrix = {}
+    i = 0
+    with open(filename) as infile:
+        line = infile.readline().strip().split()
+        n = int(line[0])
+        for line in infile:
+            emd = line.strip().split()
+            if emd[1] != "nan":
+                embedding_matrix[emd[0]] = str_list_to_float(emd[1:])
+            else:
+                print("nan error!")
+            i = i + 1
+        if(i != n):
+            print("number of nodes error!")
+    return embedding_matrix
 parser = argparse.ArgumentParser(description="test")
 parser.add_argument('-d', '--dataset', default='acm', type=str, help="Dataset")
-parser.add_argument('-m', '--model', default='PME', type=str, help='Train model')
+parser.add_argument('-m', '--model', default='Metapath2vec', type=str, help='Train model')
 parser.add_argument('-n', '--name', default='node.txt', type=str, help='Evaluation task')
 parser.add_argument('-s', '--seed', default=0, type=str, help='seed')
 args = parser.parse_args()
 
-if args.dataset == "dblp":
-    s = "a"
-    n_label = 4
-else:
-    s = "p"
-    n_label = 3
-dataset = "acm"
 filename = "./output/embedding/" + args.model + "/" + args.name
-label = {}
 label_file = "./dataset/" + args.dataset + "/label.txt"
-with open(label_file) as f:
-    for line in f:
-        i, l = line.strip().split()
-        label[int(i[1:])] = int(l)
 
-i = -1
-with open(filename) as infile:
-    line = infile.readline().strip().split()
-    embedding_matrix = np.zeros((int(line[0]), int(line[1])))
-    for line in infile:
-        emd = line.strip().split()
-        if emd[0][0] == s:
-            if emd[1] != "nan":
-                embedding_matrix[int(emd[0][1:]), :] = str_list_to_float(emd[1:])
-# _NMI = []
-# _ARI = []
-# _micro = []
-# _macro = []
-# for i in range(1, 2):
-NMI, ARI = evaluate_cluster(embedding_matrix, n_label)
-# _NMI.append(NMI)
-# _ARI.append(ARI)
+_evaluation = evaluation(args.seed, label_file)
+embedding_list = load_emd(filename)
+NMI, ARI = _evaluation.evaluate_cluster(embedding_list)
+micro, macro = _evaluation.evaluate_clf(embedding_list)
 
-micro, macro = evaluate_clf(embedding_matrix, args.seed)
-# _micro.append(micro)
-# _macro.append(macro)
 
-# auc, f1, acc = evaluate_lp(lp_embedding)
-print('<Cluster>        NMI = %.4f, ARI = %.4f' % (
-    NMI, ARI))
 
-print('<Classification>     Micro-F1 = %.4f, Macro-F1 = %.4f' % (
-    micro, macro))
+print('<Cluster>        NMI = %.4f, ARI = %.4f' % (NMI, ARI))
+
+print('<Classification>     Micro-F1 = %.4f, Macro-F1 = %.4f' % (micro, macro))
 # print('<Cluster> 		NMI = %.4f, ARI = %.4f(%.4f)' % (
 #     np.mean(_NMI), np.mean(_ARI), np.std(_ARI, ddof=1)))
 
